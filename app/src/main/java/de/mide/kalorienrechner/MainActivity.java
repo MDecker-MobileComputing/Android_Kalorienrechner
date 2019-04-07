ee3925f0c7e8cdcca5910a6a660717afd50b10ac
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-04-07 13:51:47 +0200

    Keyboard wird nach Berechnung wieder eingeklappt.

--- app/src/main/java/de/mide/kalorienrechner/MainActivity.java
+++ app/src/main/java/de/mide/kalorienrechner/MainActivity.java
@@ -1,9 +1,11 @@
 package de.mide.kalorienrechner;
 
+import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -49,9 +51,11 @@ public class MainActivity extends FragmentActivity
 
     /**
      * Event-Handler für Berechnen-Button.
+     *
+     * @param view  UI-Element (Button), das das Event ausgelöst hat.
      */
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
         int fruehstueckKilokalorien  = _energieFruehstueck.getKilokalorien();
         if (fruehstueckKilokalorien < 0) {
@@ -73,6 +77,8 @@ public class MainActivity extends FragmentActivity
 
         int summe = fruehstueckKilokalorien + mittagessenKilokalorien + abendessenKilokalorien;
         zeigeToastNachricht("Gesamtenergie: " + summe + " kCal");
+
+        keyboardEinklappen(view);
     }
 
 
@@ -85,6 +91,21 @@ public class MainActivity extends FragmentActivity
     protected void zeigeToastNachricht(String toastNachricht) {
 
         Toast.makeText(this, toastNachricht, Toast.LENGTH_LONG).show();
+    }
+
+
+    /**
+     * Virtuelles Keyboard wieder "einklappen".
+     * Lösung nach
+     * <a href="https://stackoverflow.com/a/17789187/1364368">https://stackoverflow.com/a/17789187/1364368</a>
+     *
+     * @param view  UI-Element, um Windows-Token zu erhalten.
+     */
+    public void keyboardEinklappen(View view) {
+
+        InputMethodManager imm = (InputMethodManager)
+                getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
